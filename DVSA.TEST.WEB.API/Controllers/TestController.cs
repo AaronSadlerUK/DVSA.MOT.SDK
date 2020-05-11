7ad954fb5a52634657b02af26b1982f2314d9678
Aaron <aaron@aaronsadler.co.uk>
2020-05-11 22:33:23 +0100

    Wired up single vehicle request

---
+++ DVSA.TEST.WEB.API/Controllers/TestController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DVSA.MOT.SDK.Interfaces;
+using DVSA.MOT.SDK.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DVSA.TEST.WEB.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IMotTests _motTests;
+        private IOptions<ApiKey> _apiKey;
+        public TestController(IMotTests motTests, IOptions<ApiKey> apiKey)
+        {
+            _motTests = motTests;
+            _apiKey = apiKey;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<string>> GetVehicle(string registration)
+        {
+            var vehicleDetails = await _motTests.GetSingleVehicleMotHistoryByRegistration(registration);
+
+            if (vehicleDetails != null)
+            {
+                return Ok(vehicleDetails);
+            }
+
+            return NotFound(registration);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- DVSA.TEST.WEB.API/Startup.cs
+++ DVSA.TEST.WEB.API/Startup.cs
@@ -1,31 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DVSA.MOT.SDK.Interfaces;
+using DVSA.MOT.SDK.Models;
+using DVSA.MOT.SDK.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace DVSA.TEST.WEB.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IHostingEnvironment HostingEnvironment { get; private set; }
+        public IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
+            HostingEnvironment = env;
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.Configure<ApiKey>(Configuration.GetSection("MotApi"));
+            services.AddTransient<IMotTests, MotTests>();
+            services.AddOptions();
+            services.AddLogging();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
