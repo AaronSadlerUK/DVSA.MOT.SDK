5bc64a496aaaa5a9270067bdd6eeab32ba156828
Aaron <aaron@aaronsadler.co.uk>
2020-05-13 23:11:12 +0100

    Migrated to dot net core 3.1

--- DVSA.TEST.WEB.API/Startup.cs
+++ DVSA.TEST.WEB.API/Startup.cs
@@ -1,21 +1,20 @@
 ﻿using DVSA.MOT.SDK;
-using DVSA.MOT.SDK.Interfaces;
 using DVSA.MOT.SDK.Models;
-using DVSA.MOT.SDK.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace DVSA.TEST.WEB.API
 {
     public class Startup
     {
-        public IHostingEnvironment HostingEnvironment { get; private set; }
+        public IWebHostEnvironment HostingEnvironment { get; private set; }
         public IConfiguration Configuration { get; private set; }
 
-        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             HostingEnvironment = env;
             Configuration = configuration;
@@ -24,7 +23,7 @@ namespace DVSA.TEST.WEB.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
             services.Configure<ApiKey>(Configuration.GetSection("MotApi"));
             services.AddDvlaMotSdk();
             services.AddOptions();
@@ -32,7 +31,7 @@ namespace DVSA.TEST.WEB.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -44,7 +43,14 @@ namespace DVSA.TEST.WEB.API
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
         }
     }
 }
