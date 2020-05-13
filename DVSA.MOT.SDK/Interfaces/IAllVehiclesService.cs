268130f0b0f596693376e865a3116654441bb0bc
Aaron <aaron@aaronsadler.co.uk>
2020-05-13 21:56:09 +0100

    Added remaining endpoints

--- DVSA.MOT.SDK/Interfaces/IAllVehiclesService.cs
+++ DVSA.MOT.SDK/Interfaces/IAllVehiclesService.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Threading.Tasks;
 using DVSA.MOT.SDK.Models;
 
 namespace DVSA.MOT.SDK.Interfaces
 {
     public interface IAllVehiclesService
     {
-        MotTestResponses GetAllMotTests(int page);
-        MotTestResponses GetAllMotTestsByDate(DateTime date, int page);
+        Task<ApiResponse> GetAllMotTests(int page);
+        Task<ApiResponse> GetAllMotTestsByDate(DateTime date, int page);
     }
 }
--- DVSA.MOT.SDK/Services/AllVehiclesService.cs
+++ DVSA.MOT.SDK/Services/AllVehiclesService.cs
@@ -1,19 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using DVSA.MOT.SDK.Interfaces;
 using DVSA.MOT.SDK.Models;
+using Microsoft.Extensions.Logging;
 
 namespace DVSA.MOT.SDK.Services
 {
     public class AllVehiclesService : IAllVehiclesService
     {
-        public MotTestResponses GetAllMotTests(int page)
+        private readonly ILogger<SingleVehicleService> _logger;
+        private readonly IProcessApiResponse _processApiResponse;
+
+        public AllVehiclesService(ILogger<SingleVehicleService> logger, IProcessApiResponse processApiResponse)
         {
-            throw new NotImplementedException();
+            _logger = logger;
+            _processApiResponse = processApiResponse;
         }
 
-        public MotTestResponses GetAllMotTestsByDate(DateTime date, int page)
+        public async Task<ApiResponse> GetAllMotTests(int page)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var parameters = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>(Constants.Parameters.Page, page.ToString())
+                    };
+                return await _processApiResponse.GetData(parameters);
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, ex.Message);
+                return null;
+            }
+        }
+
+        public async Task<ApiResponse> GetAllMotTestsByDate(DateTime date, int page)
+        {
+            try
+            {
+                var parameters = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>(Constants.Parameters.Date, date.ToString("yyyyMMdd")),
+                    new KeyValuePair<string, string>(Constants.Parameters.Page, page.ToString())
+                };
+                return await _processApiResponse.GetData(parameters);
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, ex.Message);
+                return null;
+            }
         }
     }
 }
