5eadccf1bfc87d4c8faee08c970e689231b9147d
Aaron <aaron@aaronsadler.co.uk>
2020-05-13 21:55:26 +0100

    Added service extension

---
+++ DVSA.MOT.SDK/AddDvlaMotSdk.cs
@@ -0,0 +1,17 @@
+﻿using DVSA.MOT.SDK.Interfaces;
+using DVSA.MOT.SDK.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DVSA.MOT.SDK
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddDvlaMotSdk(this IServiceCollection services)
+        {
+            services.AddScoped<ISingleVehicleService, SingleVehicleService>();
+            services.AddScoped<IAllVehiclesService, AllVehiclesService>();
+            services.AddScoped<IProcessApiResponse, ProcessApiResponse>();
+            return services;
+        }
+    }
+}
